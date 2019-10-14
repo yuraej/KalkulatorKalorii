9894c4762e297e8a4329836721c8e99957eadc8a
yuraej <yuraej@gmail.com>
2019-10-14 16:31:25 +0300

    update project

--- src/test/java/resources/FormulasForCalculatingCalories.java
+++
@@ -1,7 +0,0 @@
-package resources;
-
-public class FormulasForCalculatingCalories {
-    public static int formulaByMifflinForMan(int weight, int growth, int age) {
-        return (int) Math.round((10 * weight + 6.25 * growth - 5 * age + 5) * 1.375);
-    }
-}
